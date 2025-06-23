6b0f566b7cdd547c9e6c276dd88116d3943ee6a4
Matheus Rocha <matheus.rochaevs@gmail.com>
2025-06-23 14:55:31 -0300

    commit

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,9 +1,13 @@
-from warnings import deprecated
+from typing import Annotated
 
-from fastapi import APIRouter
+from database import SessionLocal
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
 from models import Users
 from passlib.context import CryptContext
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from starlette import status
 
 router = APIRouter()
 
@@ -17,8 +21,30 @@ class CreateUserRequest(BaseModel):
     password: str
     role: str
 
-@router.post("/auth/")
-async def create_user(create_user_request: CreateUserRequest):
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally: 
+        db.close()
+        
+db_dependency = Annotated[Session, Depends(get_db)]
+
+def authenticate_user(username: str, password: str, db):
+    user = db.query(Users).filter(
+        Users.username == username
+    ).first()
+    
+    if not user:
+        return False
+    
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    
+    return True
+
+@router.post("/auth/", status_code=status.HTTP_201_CREATED)
+async def create_user(create_user_request: CreateUserRequest, db: db_dependency):
     create_user_model = Users(
         email=create_user_request.email,
         username=create_user_request.username,
@@ -29,9 +55,23 @@ async def create_user(create_user_request: CreateUserRequest):
         is_active=True
     )
     
+    db.add(create_user_model)
+    db.commit()
+    
     
     return create_user_model
 
 """
 Se você tentar passar isso diretamente para Users(**...), o Python tentaria encontrar um parâmetro chamado password na classe Users, mas ela espera hashed_password. Além disso, o campo is_active em Users não seria preenchido pelo dicionário, o que causaria um erro se ele não tiver um valor padrão ou for obrigatório.
 """
+
+@router.post("/token")
+async def login_for_acess_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], 
+    db: db_dependency
+):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return "Failed Authentication"
+    
+    return 'Successful Authentication'
