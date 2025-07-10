1c9270c392d6e58b6c3eb2d7ab6efbd032e5eb58
Matheus Rocha <matheus.rochaevs@gmail.com>
2025-07-10 14:00:47 -0300

    commit

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -134,13 +134,13 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
         user_role: str = payload.get('role')
         
         if username is None or user_id is None:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user 1')
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
         
         return {'username': username, 'id': user_id, 'user_role': user_role}
     
     except JWTError as e:
         print(f'Erro no JWT: {str(e)}')  # ou use logging
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user 2')
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
      
         
 @router.post("/", status_code=status.HTTP_201_CREATED)
